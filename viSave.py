eb697c6189da304142c485b024e0b66e34243493
zigdog97 <zigdog97@cox.net>
2015-05-22 11:02:16 -0400

    commenting and variable name changes
    Fixed some variables name that were not clear/used for another program.
    commented in uses for functions and variables!

--- viSave.py
+++ viSave.py
@@ -1,21 +1,23 @@
 import pickle
-
+#make sure to use virusCreate()
+#VIRUS_FILENAME is the variable holding the file where virusList is saved
 VIRUS_FILENAME = "virusList.pkl"
-virusSaveList = dict()
-
+#dictionary of viruses
+virusList = dict()
+# add viruses To virusList and saves it
 def virusAdd():
-    global virusSaveList
+    global virusList
     virus = virusCreate()
-    virusSaveList[virus.name] = virus
+    virusList[virus.name] = virus
     saveGame()
-    return virusSaveList
-    
-def loadGame():
+    return virusList
+#used at the beginning of main() to load all viruses to program from VIRUS_FILENAME
+def loadVirus():
     with open(VIRUS_FILENAME, 'rb') as saveVirus:
         state = pickle.load(saveVirus)
     return state
-
-def saveGame():
-    global gameState
+# used whenever you need to save something this function can be used to save a virus to list at anytime.
+def saveVirus():
+    global virusList
     with open(VIRUS_FILENAME, 'wb') as saveVirus:
-        pickle.dump(virusSaveList,saveVirus)
+        pickle.dump(virusList,saveVirus)
